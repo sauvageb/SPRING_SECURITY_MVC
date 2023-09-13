c2e444396ec8f4c77e2445bb1d41f3325fb391a4
sauvageb <51415938+sauvageb@users.noreply.github.com>
2023-09-13 12:04:16 +0200

    Delete demo-spring-security-restful directory

--- demo-spring-security-restful/src/main/java/com/example/demospringsecurityrestful/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demospringsecurityrestful.repository;
-
-import com.example.demospringsecurityrestful.repository.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
